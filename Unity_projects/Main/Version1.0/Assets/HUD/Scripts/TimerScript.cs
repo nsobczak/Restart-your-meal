255127d6f4909440956df2e8759f18b5bdf2716d
nsobczak <nicolas.sobczak@orange.fr>
2017-11-19 17:20:54 +0000

    v1.0: - add hud - add menu - add playerController v1.0: - add gitignore v1.0: - remove library folder

---
+++ Unity_projects/Main/Version1.0/Assets/HUD/Scripts/TimerScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Timers;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    private static String Timer;
+
+    [SerializeField] private float _startTime;
+    private Text _textTime;
+
+    void Awake()
+    {
+        _startTime = Time.time;
+    }
+
+    void Start()
+    {
+        _textTime = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        if (true)
+        {
+            float currentTime = Time.time - _startTime;
+
+            int minutes = (int) (currentTime / 60);
+            int seconds = (int) (currentTime % 60);
+            int fraction = (int) ((currentTime * 100) % 100);
+
+            Timer = String.Format("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction);
+            _textTime.text = Timer;
+        }
+    }
+}
