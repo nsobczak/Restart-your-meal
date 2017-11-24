7efad671cab7dc0bf6a931dcc13ae55e7287bd1b
nsobczak <nicolas.sobczak@orange.fr>
2017-11-24 11:36:45 +0000

    - v1.0: add spawning point (not working yet)

---
+++ Unity_projects/Main/Version1.0/Assets/GameController/Scripts/GhostGameOver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostGameOver : MonoBehaviour
+{
+    #region OnTrigger
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("collision player-ghost detected");
+            //            Destroy(col.gameObject);
+
+            GameController.IsGameOver = true;
+        }
+    }
+    #endregion
+}
