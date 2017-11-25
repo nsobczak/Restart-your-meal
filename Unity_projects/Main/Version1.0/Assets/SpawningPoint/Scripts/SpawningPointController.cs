f89ee4b6cacd59c9a5796512eeb246e07796bbc9
nsobczak <nicolas.sobczak@orange.fr>
2017-11-25 15:04:14 +0100

    - v1.0: Add randomly positionned spawning points

--- Unity_projects/Main/Version1.0/Assets/SpawningPoint/Scripts/SpawningPointController.cs
+++ Unity_projects/Main/Version1.0/Assets/SpawningPoint/Scripts/SpawningPointController.cs
@@ -12,8 +12,16 @@ public class SpawningPointController : MonoBehaviour
     #region Parameters
 
     [SerializeField] private GameObject spawningPointPrefab;
-    [SerializeField] private float _SPAWNING_POINT_OFFESET_ = 5f;
-    [SerializeField] private int spawningPointCount; //may be useless if we create a new point each time we restart level
+    [SerializeField] private float _SPAWNING_POINT_OFFSET_ = 6f;
+    [SerializeField] private int _RADIUS_MIN_ = 30;
+    [SerializeField] private int _RADIUS_MAX_ = 50;
+
+
+    [SerializeField] private bool instantiateNewSpawningPoint;
+
+    [SerializeField]
+    private int spawningPointCount; //may be useless if we create a new point each time we restart level
+
     private GameObject player;
 
     private GameObject currentSpawningPoint;
@@ -48,22 +56,36 @@ public class SpawningPointController : MonoBehaviour
 
     #region Methods
 
+    //Compute position for new spawning point in spawningPointGenerator
+    private Vector3 computeNewPosition()
+    {
+        int radius = Random.Range(_RADIUS_MIN_, _RADIUS_MAX_);
+        float angle = Random.Range(0, 2 * Mathf.PI);
+        Vector3 newPosition = new Vector3(transform.position.x + radius * Mathf.Cos(angle),
+            transform.position.y + _SPAWNING_POINT_OFFSET_,
+            transform.position.z + radius * Mathf.Sin(angle));
+        return newPosition;
+    }
+
+
     private void AddSpawningPoint()
     {
         spawningPointCount++;
-        Vector3 newSpawningPointPostion = new Vector3();
-        Vector3 newSpawningPointRotation = new Vector3();
+        Vector3 newSpawningPointPostion = computeNewPosition();
+        Debug.Log("newSpawningPointPostion: " + newSpawningPointPostion);
         currentSpawningPoint = Instantiate(spawningPointPrefab, newSpawningPointPostion,
-            Quaternion.Euler(newSpawningPointRotation));
+            Quaternion.identity);
+        currentSpawningPoint.transform.parent = transform;
 
-        currentSpawningPoint.GetComponent<MeshRenderer>().gameObject.SetActive(false); //make it invisible
+//        currentSpawningPoint.GetComponent<MeshRenderer>().gameObject.SetActive(false); //make it invisible
     }
 
 
     private void SpawnPlayer()
     {
-        player.transform.position = new Vector3(transform.position.x, transform.position.y,
-            transform.position.z - _SPAWNING_POINT_OFFESET_);
+        player.transform.position = new Vector3(currentSpawningPoint.transform.position.x,
+            currentSpawningPoint.transform.position.y,
+            currentSpawningPoint.transform.position.z);
     }
 
     #endregion
@@ -73,13 +95,23 @@ public class SpawningPointController : MonoBehaviour
 
     void Start()
     {
+        instantiateNewSpawningPoint = false;
+
         spawningPointCount = 0;
+        AddSpawningPoint();
+
         player = GameObject.FindGameObjectWithTag("Player");
-        Debug.Log("player: " + player);
         SpawnPlayer();
     }
 
-//    void Update()
-//    {
-//    }
+    void Update()
+    {
+        if (instantiateNewSpawningPoint)
+        {
+            AddSpawningPoint();
+            instantiateNewSpawningPoint = false;
+        }
+//        Debug.Log("Random.Range(_RADIUS_MIN_, _RADIUS_MAX_) = " + Random.Range(_RADIUS_MIN_, _RADIUS_MAX_));
+//        Debug.Log("Random.Range(0, Mathf.PI) = " + Random.Range(0, Mathf.PI));
+    }
 }
